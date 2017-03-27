18c61aac98b75ce6791e3ef92688d33618ad2f27
Patryk Dzwoniarski <Patryk Dzwoniarski>
2017-03-27 19:25:35 +0200

    new WPF project with face recognition

---
+++ SFR/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using System.Windows.Threading;
+using System.Runtime.InteropServices;
+
+
+
+namespace SFR
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Capture capture;
+        private CascadeClassifier haarCascade;
+        DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+    }
+}
